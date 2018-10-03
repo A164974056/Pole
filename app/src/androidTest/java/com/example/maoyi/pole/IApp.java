4f4c98a901ccf725cf67ed4a080e1ceea3005eeb
maoyi <maoyi.f.mao@outlook.com>
2018-10-03 19:22:10 +0800

    1. 创建了项目 2. 终于可以打开了..

---
+++ app/src/androidTest/java/com/example/maoyi/pole/IApp.java
@@ -0,0 +1,4 @@
+package com.example.maoyi.pole;
+
+public interface IApp {
+}
--- app/src/main/java/com/example/maoyi/pole/MainActivity.java
+++ app/src/main/java/com/example/maoyi/pole/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.maoyi.pole;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -11,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
+
+    private homeFragment fragment1;
+    private hotFragment fragment2;
+    private personalFragment fragment3;
+    private Fragment[] fragments;
+    private int lastShowFragment = 0;
+
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -18,14 +28,26 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    if (lastShowFragment != 0) {
+                        switchFrament(lastShowFragment, 0);
+                        lastShowFragment = 0;
+                    }
                     return true;
+
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    if (lastShowFragment != 1) {
+                        switchFrament(lastShowFragment, 1);
+                        lastShowFragment = 1;
+                    }
                     return true;
+
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                if (lastShowFragment != 2) {
+                    switchFrament(lastShowFragment, 2);
+                    lastShowFragment = 2;
+                }
+                return true;
+
             }
             return false;
         }
@@ -36,9 +58,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        initFragments();
     }
 
+    public void switchFrament(int lastIndex, int index) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.hide(fragments[lastIndex]);
+        if (!fragments[index].isAdded()) {
+            transaction.add(R.id.content, fragments[index]);
+        }
+        transaction.show(fragments[index]).commitAllowingStateLoss();
+    }
+
+    private void initFragments() {
+        fragment1 = new homeFragment();
+        fragment2 = new hotFragment();
+        fragment3 = new personalFragment();
+        fragments = new Fragment[]{fragment1, fragment2, fragment3};
+        lastShowFragment = 0;
+        getSupportFragmentManager().beginTransaction().add(R.id.content, fragment1).show(fragment1).commit();
+    }
 }
